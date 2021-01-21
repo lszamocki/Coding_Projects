df3deb2f0f5a702acca28330099405c1d7e43e46
Leo Szamocki <lszamocki@gmail.com>
2021-01-20 20:37:27 -0600

    first project

--- hello.js
+++ hello.js
@@ -1,4 +1,11 @@
-console.log('hello world');
+const str = 'hi there world';
+const helloworld = (str) => {
+    for (let i = 0; i < 10; i ++) {
+        console.log(str);
+    }
+}
+helloworld(str);
+
 
 
 
